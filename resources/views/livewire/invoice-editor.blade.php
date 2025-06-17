6a0931e3b47106e7649ca1fa9948e8860c32bf61
Keaton Murray <murraykeaton422@gmail.com>
2025-06-17 15:09:24 -0400

    Restructured Livewire Components

---
+++ resources/views/livewire/invoice-editor.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="row">
+        <div class="col-12 col-md-6 mt-md-1 mt-4 mb-md-auto mb-4">
+            @include('livewire.partials.invoice-form')
+        </div>
+        
+        <div class="col-12 col-md-6 d-md-block d-none">
+            @include('livewire.partials.invoice-pdf')
+        </div>
+    </div>
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,25 +1,9 @@
 @extends('layout')
 
 @section('content')
-    <div class="container=luid py-md-5 py-4 px-md-5 px-4">
+    <div class="container-fluid py-md-5 py-4 px-md-5 px-4">
         <h2 class="fw-bold">Create Invoice</h2>
         <p class="text-muted fw-bold small">Generate professional invoices in seconds. No signup required</p>
-        <div class="row">
-            <div class="col-12 col-md-6 mt-md-1 mt-4 mb-md-auto mb-4">
-                <div class="card border-0 shadow-sm">
-                    <div class="card-body">
-                        <livewire:invoice-form />
-                    </div>
-                </div>
-            </div>
-            
-            <div class="col-12 col-md-6 d-md-block d-none">
-                <div class="card border-0 shadow-sm">
-                    <div class="card-body">
-                        <livewire:invoice-pdf />
-                    </div>
-                </div>
-            </div>
-        </div>
+        <livewire:invoice-editor />
     </div>
 @endsection
